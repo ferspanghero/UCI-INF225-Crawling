53b4778620ed59f1469e1e5915f0ce7abc073627
ferspanghero <ferspanghero@WIN-NMCGV4KCMID>
2015-02-02 02:57:44 -0800

    Merged @Daized Pull Request (#2): created initial draft version of Crawler and CrawlerController classes

--- src/crawling/ui/console/Program.java
+++ src/crawling/ui/console/Program.java
@@ -1,8 +1,18 @@
 package crawling.ui.console;
 
+import crawling.core.*;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");	
+		CrawlerController controller = new CrawlerController();
+		
+		try {
+			// TODO: see how logger can be injected into the controller
+			// TODO: see how the Crawler can be injected into the controller (ICrawlerBuilder?)
+			controller.Run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
