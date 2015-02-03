19250dfdd04cbe767b6963b7dbd0b1660887b26e
ferspanghero <ferspanghero@169.234.213.77>
2015-02-03 04:39:59 -0800

    Partially implemented DefaultPagesProcessor;
    Partially implemented DefaultPagesProcessorTest (though it is still not
    working because of an error I haven't figured out yet).

--- src/crawling/core/Crawler.java
+++ src/crawling/core/Crawler.java
@@ -10,13 +10,11 @@ import edu.uci.ics.crawler4j.url.*;
  * Represents a crawler that visits and collects information about web pages
  */
 public class Crawler extends WebCrawler {
-	public Crawler(IPagesRepository repository, IPageTextParser parser) {
+	public Crawler(IPagesRepository repository) {
 		_repository = repository;
-		_parser = parser;
 	}
 
 	private IPagesRepository _repository;
-	private IPageTextParser _parser;
 
 	private final static Pattern FILTERS = Pattern
 			.compile(".*(\\.(css|js|bmp|gif|jpe?g"
