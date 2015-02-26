12d918796e53d3795d11c6a03b410fd11f5e6191
Daized <roelandsh@gmail.com>
2015-02-26 01:10:54 -0800

    Finished search engine implementation. Only relevance scoring optimizations should be left.

--- src/servlet/Ranker.java
+++ src/servlet/Ranker.java
@@ -1,28 +1,50 @@
 package servlet;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
+
+import searchengine.core.Tokenizer;
 
 public class Ranker {
 	
-	private String[] query;
-	private int numOfDocs;
-	private Map<Integer, Double> urlCosineSimilarity; //Integer is the docID
+	private List<String> query;
+	public int numOfDocs;
+	public int resultsPerPage;
 	
 	/*
 	 * Constructor takes a query string
 	 */
 	
-	public Ranker(String query){
+	public Ranker(String query, int resultsPerPage) {
 		this.query = tokenizeQuery(query);
-		this.urlCosineSimilarity = new HashMap<Integer, Double>();
+		this.numOfDocs = 0;
+		this.resultsPerPage = resultsPerPage;
+		// Loads the MySQL driver
+		try {
+		Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	public static String[] tokenizeQuery(String query){
+	/*
+	 * Helper function for constructor, tokenizes query
+	 */
+	
+	public static List<String> tokenizeQuery(String query){
 		
-		
-		
-		return null;
+		Tokenizer tokenize = new Tokenizer(query);
+		List<String> data = new ArrayList<String>(10);
+		while (tokenize.processNextToken()){
+			
+			data.add(tokenize.getCurrentToken());
+		}
+		return data;
 	}
 	
 	/*
@@ -31,62 +53,52 @@ public class Ranker {
 	 * HashMap.
 	 */
 	
-	public void search() throws SQLException{
+	public List<String> search(int page) throws SQLException{
 		
-		
-		
+		List<String> results = new ArrayList<String>(resultsPerPage);
+		try (Connection connection = getConnection()) {
+			// ResultSet.TYPE_SCROLL_SENSITIVE tells the driver to consider altered records since the last page was read
+			// ResultSet.CONCUR_READ_ONLY tells the driver to create a read-only result set
+			try (Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+				
+				StringBuilder build = new StringBuilder();
+				String sql;
+
+				build.append("select P.URL, SUM(WP.TFIDF) as totalTfidf from wordspages WP inner join pages P on WP.PageId = P.Id where wordId in (select Id from words where Word in (");
+				for (String s: query){
+					build.append("\"");					
+					build.append(s);
+					build.append("\"");
+					if (query.indexOf(s) == query.size() - 1)
+						break;
+					build.append(",");
+				}
+				build.append(")) group by WP.PageId order by totalTfidf desc");
+				sql = build.toString();
+
+				try (ResultSet resultSet = statement.executeQuery(sql)) {
+					
+					resultSet.absolute((page-1)*resultsPerPage);
+					int i = 0;
+					while (resultSet.next() && i < resultsPerPage){
+						results.add(resultSet.getString("URL"));
+						i++;
+					}
+					
+					resultSet.last();
+					numOfDocs = resultSet.getRow();
+					resultSet.close();
+				}
+			}
+		}
+		return results;
 	}	
 	
-	/*
-	 * This method sorts our search results by the computed cosine similarity and returns
-	 * a list of URL strings based upon the doc ID.
-	 */
-	
-	public List<String> getSearchResults(){
-		
-		TreeMap<Integer, Double> sorted = sortDocs(urlCosineSimilarity);
-		ArrayList<String> sortedDocs = new ArrayList<>();
-		for (Map.Entry<Integer, Double> entry : sorted.entrySet()){
-			
-		}
-
-		
-		//Record the size of arraylist while we are are it
-		this.numOfDocs = sortedDocs.size();
-		
-		return sortedDocs;
-	}
-	
-	/*
-	 * Helper function to sort HashMap into TreeMap
-	 */
-	
-	public static TreeMap<Integer, Double> sortDocs(Map<Integer, Double> unsorted){
-		//Now that we have counted all the terms, we must sort our hash values to print most frequent terms
-		DoubleComparator compare = new DoubleComparator(unsorted);
-		TreeMap<Integer, Double> sortedTermFrequency = new TreeMap<Integer, Double>(compare);
-		sortedTermFrequency.putAll(unsorted);
-		return sortedTermFrequency;
-	}
-	
-	/*
-	 * Here is our comparator for the TreeMap
-	 */
-	
-	public final static class DoubleComparator implements Comparator<Integer> {
-	    Map<Integer, Double> map;
-	    //Constructor
-	    public DoubleComparator(Map<Integer,Double> construct){
-	        this.map = construct;
-	    }
-	    //Implementing compare method, does not return 0 because we do not want to merge keys
-	    public int compare(Integer x, Integer y) {
-	        if (map.get(x) >= map.get(y)) {
-	            return -1;
-	        } else {
-	            return 1;
-	        } 
-	    }
+	private Connection getConnection() throws SQLException {
+		// TODO: make connection parameters configurable
+		// useServerPrepStmts=false tells MySQL to handle server-side prepared statements locally
+		// rewriteBatchedStatements=true tells MySQL to pack as many queries as possible into a single network packet
+		return DriverManager.getConnection("jdbc:mysql://localhost:3306/ucicrawling?user=root&password=password&useServerPrepStmts=false&rewriteBatchedStatements=true&useUnicode=true&characterEncoding=UTF-8");
 	}
 	 
 	
--- src/servlet/SearchEngineServlet.java
+++ src/servlet/SearchEngineServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.Map.Entry;
 
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/SearchEngineServlet")
 public class SearchEngineServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private String query;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -33,10 +35,35 @@ public class SearchEngineServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		
 		response.setContentType("text/html");
-		Ranker ranked = new Ranker((String) request.getAttribute("q"));
-	    List<String> listData = ranked.getSearchResults(); // however you get the data
-	    // set the attribute in the request to access it on the JSP
+		int resultsPerPage = 20;
+		Ranker ranked;
+
+		if (!(request.getParameter("q") == null)){
+			query = request.getParameter("q");
+			ranked = new Ranker(query, resultsPerPage);
+		}
+		else {
+				ranked = new Ranker(query, resultsPerPage);
+		}
+		
+	    int page = 1;
+	    if(request.getParameter("page") != null)
+            page = Integer.parseInt(request.getParameter("page"));
+	    List<String> listData = null;
+	    
+	    try {  listData = ranked.search(page); // however you get the data*/
+	    } catch (SQLException e){
+	    	e.printStackTrace();
+	    }
+	    
+	    int numOfPages = (int) Math.ceil(ranked.numOfDocs * 1.0 / resultsPerPage);
+	    int listStartNumber = (page-1)*resultsPerPage + 1;
+	    // set the attributes in the request to access it on the JSP
+	    request.setAttribute("query", query);
 	    request.setAttribute("listData", listData);
+	    request.setAttribute("numOfPages", numOfPages);
+	    request.setAttribute("listStart", listStartNumber);
+	    request.setAttribute("currentPage", page);
 	    RequestDispatcher rd = getServletContext().getRequestDispatcher("/results.jsp");
 	    rd.forward(request, response);
 
